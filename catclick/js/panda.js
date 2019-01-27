dca47220084db0ffb78c7d01733cd540c98d7dcb
oyiooi <38151942+oyiooi@users.noreply.github.com>
2019-01-27 10:05:49 +0800

    项目要求改变
    视觉
    该应用程序应该显示
    
    一个包含至少 5 只猫的列表（按名称列出）
    一块用于显示所选的猫的区域
    在猫显示区域，应该会显示以下内容
    
    猫的名称
    猫的图片
    显示点击数的文本
    
    交互
    点击列表中的猫名称时，猫显示区域应该更新以显示所选的猫的数据。
    猫区域内的点击数应该对每只猫具有唯一性，并且应该在点击猫的图片时增加

--- catclick/js/panda.js
+++ catclick/js/panda.js
@@ -1,9 +1,37 @@
-let clickNumber=0,
-    image=document.querySelector('img');
+let dataArray=[{name:'bird',url:'picture/bird.jpg'},
+               {name:'cat',url:'picture/cat.jpg'},
+               {name:'dog',url:'picture/dog.jpg'},
+               {name:'pig',url:'picture/pig.jpg'},
+               {name:'panda',url:'picture/panda.jpg'}];
 
-    image.addEventListener('click',function(){
-        clickNumber++;
-        document.querySelector('p').innerText='clicknumber:'+clickNumber;
-    })
+let plist=document.createElement('ol');
+document.body.appendChild(plist);
 
-    
+let disDiv=document.createElement('div');
+document.body.appendChild(disDiv);
+
+let disp=document.createElement('p');
+disDiv.appendChild(disp);
+
+let disimg=document.createElement('img');
+disDiv.appendChild(disimg);
+
+for(let i=0;i<dataArray.length;i++){
+    let item=document.createElement('li');
+    plist.appendChild(item);
+
+    item.textContent=dataArray[i].name;
+
+    item.addEventListener('click',(function (copyi) {
+        return function (){
+            disp.textContent='name:'+dataArray[copyi].name+' clickTime: ';
+            disimg.src=dataArray[copyi].url;
+            let clickTime=0;
+
+            disimg.addEventListener('click',function(){
+                clickTime++;
+                disp.textContent='name:'+dataArray[copyi].name+' clickTime: '+clickTime;
+            })
+        };
+    })(i));
+}
